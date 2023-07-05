3defe1acd1d08b1b8cce0a57afc536e49ac49e6e
Leonardo F.C <leonardofc2002@gmail.com>
2023-07-05 15:21:54 -0300

    feat: song icon

--- src/components/Home/SongsList.tsx
+++ src/components/Home/SongsList.tsx
@@ -1,7 +1,7 @@
 import { useStore } from '@nanostores/react';
 import { computed, ReadableAtom } from 'nanostores';
 import { FlatList, TouchableOpacity } from 'react-native';
-import { Background, Text } from '~/components/shared/Themed';
+import { Background, Icon, Text } from '~/components/shared/Themed';
 import { Playback, Song } from '~/services/songs';
 
 export default function SongsList(p: {
@@ -13,28 +13,21 @@ export default function SongsList(p: {
       <FlatList
         data={p.playlist}
         renderItem={({ item }) => (
-          <SongsItem song={item} onPress={() => p.$selected.set(item)} />
+          <SongsItem
+            song={item}
+            $isSelected={computed(
+              p.$selected,
+              (selected) => selected?.id === item.id,
+            )}
+            onPress={() => p.$selected.set(item)}
+          />
         )}
       />
     </Background>
   );
 }
 
-function SongsItem(p: { song: Song; onPress: () => void }) {
-  return (
-    <TouchableOpacity
-      onPress={p.onPress}
-      className='w-full flex-row items-center rounded p-4'>
-      <Text numberOfLines={2} className={`flex-shrink`}>
-        <Text className={`font-bold`}>{p.song.songName}</Text>
-        {' - '}
-        {p.song.authorName}
-      </Text>
-    </TouchableOpacity>
-  );
-}
-
-/* function SongsItem(p: {
+function SongsItem(p: {
   song: Song;
   $isSelected: ReadableAtom<boolean>;
   onPress: () => void;
@@ -55,4 +48,4 @@ function SongsItem(p: { song: Song; onPress: () => void }) {
       </Text>
     </TouchableOpacity>
   );
-} */
+}
